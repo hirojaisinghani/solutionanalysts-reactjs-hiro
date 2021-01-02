dc5f29dc5ad8e78dce1c67155eb8b807abd9d64d
hirojaisinghani <hiro.jaisinghani@solutionanalysts.com>
2021-01-02 12:06:05 +0530

    [feature]: useState and useEffect for functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import leaderboardData from './leaderboardData.json';
 import { formatLeaderboardData } from './utils'
+import { INTERVAL } from './constants';
 
 import './App.css';
 
 function App() {
   // usState hook local state 
-  const [data, setData] = useState()
-  console.log(formatLeaderboardData([...leaderboardData]));
+  const [data, setData] = useState(formatLeaderboardData([...leaderboardData]));
+  
+
+  // Similar to componentDidMount and componentDidUpdate
+  useEffect(() => {
+    // set interval for every second to update 
+    const interval = setInterval( () => {
+      setData(formatLeaderboardData([...leaderboardData]));
+    }, INTERVAL);
+    return () => clearInterval(interval);
+  })
   return (
     <>
     </>
