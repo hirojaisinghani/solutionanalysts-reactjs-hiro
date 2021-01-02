cff13a8f6cb43f676094f7ac9c10cd9220166892
hirojaisinghani <hiro.jaisinghani@solutionanalysts.com>
2021-01-02 11:28:15 +0530

    [feature]: Leaderboard input data getting and formatting  on root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import leaderboardData from './leaderboardData.json';
+import { formatLeaderboardData } from './utils'
+
 import './App.css';
 
 function App() {
+  // usState hook local state 
+  const [data, setData] = useState()
+  console.log(formatLeaderboardData([...leaderboardData]));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    </>
   );
 }
 
