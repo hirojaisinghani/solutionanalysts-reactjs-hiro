0d87d3d95d123c8c70ec6d115df2fe6a0833c91b
hirojaisinghani <hiro.jaisinghani@solutionanalysts.com>
2021-01-02 12:07:13 +0530

    [feature]: redux and react-redux provider added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+import rootReducer from './reducers/rootReducer';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
